9964ba488644ad8ab8d4d8bd1c8d0d076d0c7797
Tomasz Nowak <mychaty@gmail.com>
2022-10-22 09:37:45 +0200

    Feature/create basic layout (#1)
    * Draft of profile section
    
    * Add styling to group button
    
    * Add proper navigation bar
    
    * Move Navigation Bar to separate widget
    
    * Replaced navigation bar. Split app into tabs.
    
    * Extract navigation bar to separate file.
    
    There is another bottom nav bar for testing purposes
    
    * Add little theming. Display brew page.
    
    * Run on ios allowing app to open from screen
    
    * Add icon pack
    
    * Add brew setup section
    
    * Init remaining parts of brewing page

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,20 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'widgets/stylish_navigation_bar.dart';
+import 'widgets/animated_navigation_bar.dart';
+
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '4-6 method',
-      home: Scaffold(
-        appBar: AppBar(title: const Text('4-6 method')),
-        body: const Center(child: Text("Hello Coffee!")),
+      title: '4-6 Method',
+      home: Method46(),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.brown)
+            .copyWith(secondary: Colors.amber),
       ),
     );
+  }
+}
+
+class Method46 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedNavigationBar(
+      title: '4-6 Method',
+    );
+    //return StylishNavigationBar();
   }
 }
---
+++ app/lib/widgets/brew_page.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:group_button/group_button.dart';
+
+class BrewPage extends StatefulWidget {
+  const BrewPage({super.key});
+
+  @override
+  State<BrewPage> createState() => _BrewPageState();
+}
+
+class _BrewPageState extends State<BrewPage> {
+  var brewSettingSwitchValue = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          height: 150,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        child: Text('Coffee'),
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                    Switch(
+                      activeTrackColor: Colors.green,
+                      inactiveTrackColor: Colors.green,
+                      activeColor: Colors.amber,
+                      inactiveThumbColor: Colors.orangeAccent,
+                      value: brewSettingSwitchValue,
+                      onChanged: (value) =>
+                          setState(() => brewSettingSwitchValue = value),
+                    ),
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+                      child: Container(
+                        height: 50,
+                        width: 50,
+                        child: Text('Water'),
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 60, vertical: 10),
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      child: Text('Grinder'),
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 60, vertical: 10),
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      child: Text('Ground Size'),
+                      color: Colors.blueGrey,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Container(
+          height: 80,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Brewing Profile'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  GroupButton(
+                    buttons: ['sweet', 'balanced', 'acidic'],
+                    options: GroupButtonOptions(
+                      selectedShadow: const [],
+                      groupingType: GroupingType.wrap,
+                      elevation: 0,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+        Container(
+          height: 400,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Text('Brew'),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      Text('Water'),
+                      Text('0 g'),
+                      Text('100 g'),
+                      Text('200 g'),
+                      Text('300 g'),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Text('Time'),
+                      Text('0:00 min'),
+                      Text('0:45 min'),
+                      Text('1:15 min'),
+                      Text('2:00 min'),
+                    ],
+                  ),
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text('Progress'),
+                      FaIcon(FontAwesomeIcons.mugHot)
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
