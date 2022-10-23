c8a0f5ed63eb872541594731309924383f12a226
Tomasz Nowak <mychaty@gmail.com>
2022-10-23 09:56:50 +0200

    Removed extra nav bar. Refactor Animated nav bar.

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,8 +1,15 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/rendering.dart';
 
-import 'widgets/stylish_navigation_bar.dart';
-import 'widgets/animated_navigation_bar.dart';
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+import 'widgets/navigation_screen.dart';
+import 'widgets/brew_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,12 +32,173 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Method46 extends StatelessWidget {
+class Method46 extends StatefulWidget {
+  @override
+  State<Method46> createState() => _Method46State();
+}
+
+class _Method46State extends State<Method46> with TickerProviderStateMixin {
+  final autoSizeGroup = AutoSizeGroup();
+  var _bottomNavIndex = 0; //default index of a first screen
+
+  late AnimationController _fabAnimationController;
+  late AnimationController _borderRadiusAnimationController;
+  late Animation<double> fabAnimation;
+  late Animation<double> borderRadiusAnimation;
+  late CurvedAnimation fabCurve;
+  late CurvedAnimation borderRadiusCurve;
+  late AnimationController _hideBottomBarAnimationController;
+
+  final iconList = [
+    {Icons.coffee_maker, 'Brew'},
+    {Icons.settings, 'Settings'},
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    final systemTheme = SystemUiOverlayStyle.dark.copyWith(
+      systemNavigationBarColor: HexColor('#373A36'),
+      systemNavigationBarIconBrightness: Brightness.light,
+    );
+    SystemChrome.setSystemUIOverlayStyle(systemTheme);
+
+    _fabAnimationController = AnimationController(
+      duration: Duration(milliseconds: 500),
+      vsync: this,
+    );
+    _borderRadiusAnimationController = AnimationController(
+      duration: Duration(milliseconds: 500),
+      vsync: this,
+    );
+    fabCurve = CurvedAnimation(
+      parent: _fabAnimationController,
+      curve: Interval(0.5, 1.0, curve: Curves.fastOutSlowIn),
+    );
+    borderRadiusCurve = CurvedAnimation(
+      parent: _borderRadiusAnimationController,
+      curve: Interval(0.5, 1.0, curve: Curves.fastOutSlowIn),
+    );
+
+    fabAnimation = Tween<double>(begin: 0, end: 1).animate(fabCurve);
+    borderRadiusAnimation = Tween<double>(begin: 0, end: 1).animate(
+      borderRadiusCurve,
+    );
+
+    _hideBottomBarAnimationController = AnimationController(
+      duration: Duration(milliseconds: 200),
+      vsync: this,
+    );
+
+    Future.delayed(
+      Duration(seconds: 1),
+      () => _fabAnimationController.forward(),
+    );
+    Future.delayed(
+      Duration(seconds: 1),
+      () => _borderRadiusAnimationController.forward(),
+    );
+  }
+
+  bool onScrollNotification(ScrollNotification notification) {
+    if (notification is UserScrollNotification &&
+        notification.metrics.axis == Axis.vertical) {
+      switch (notification.direction) {
+        case ScrollDirection.forward:
+          _hideBottomBarAnimationController.reverse();
+          _fabAnimationController.forward(from: 0);
+          break;
+        case ScrollDirection.reverse:
+          _hideBottomBarAnimationController.forward();
+          _fabAnimationController.reverse(from: 1);
+          break;
+        case ScrollDirection.idle:
+          break;
+      }
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return AnimatedNavigationBar(
-      title: '4-6 Method',
+    return Theme(
+      data: ThemeData.dark(),
+      child: Scaffold(
+        extendBody: true,
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              "4-6 Method",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          backgroundColor: HexColor('#373A36'),
+        ),
+        body: NotificationListener<ScrollNotification>(
+          onNotification: onScrollNotification,
+          child: _bottomNavIndex == 0
+              ? Container(
+                  color: Colors.brown,
+                  child: BrewPage(),
+                )
+              : NavigationScreen(iconList[_bottomNavIndex].first as IconData),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: HexColor('#FFA400'),
+          child: FaIcon(FontAwesomeIcons.mugHot),
+          onPressed: () {
+            _fabAnimationController.reset();
+            _borderRadiusAnimationController.reset();
+            _borderRadiusAnimationController.forward();
+            _fabAnimationController.forward();
+          },
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: AnimatedBottomNavigationBar.builder(
+          itemCount: iconList.length,
+          tabBuilder: (int index, bool isActive) {
+            final color = isActive ? HexColor('#FFA400') : Colors.white;
+            return Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  iconList[index].first as IconData,
+                  size: 24,
+                  color: color,
+                ),
+                const SizedBox(height: 4),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: AutoSizeText(
+                    iconList[index].last as String,
+                    maxLines: 1,
+                    style: TextStyle(color: color),
+                    group: autoSizeGroup,
+                  ),
+                )
+              ],
+            );
+          },
+          backgroundColor: HexColor('#373A36'),
+          activeIndex: _bottomNavIndex,
+          splashColor: HexColor('#FFA400'),
+          notchAndCornersAnimation: borderRadiusAnimation,
+          splashSpeedInMilliseconds: 300,
+          notchSmoothness: NotchSmoothness.defaultEdge,
+          gapLocation: GapLocation.center,
+          leftCornerRadius: 32,
+          rightCornerRadius: 32,
+          onTap: (index) => setState(() => _bottomNavIndex = index),
+          hideAnimationController: _hideBottomBarAnimationController,
+          shadow: BoxShadow(
+            offset: Offset(0, 1),
+            blurRadius: 12,
+            spreadRadius: 0.5,
+            color: HexColor('#FFA400'),
+          ),
+        ),
+      ),
     );
-    //return StylishNavigationBar();
   }
 }
---
+++ app/lib/widgets/navigation_screen.dart
@@ -0,0 +1,93 @@
+import 'package:circular_reveal_animation/circular_reveal_animation.dart';
+import 'package:flutter/material.dart';
+
+class NavigationScreen extends StatefulWidget {
+  final IconData iconData;
+
+  NavigationScreen(this.iconData) : super();
+
+  @override
+  _NavigationScreenState createState() => _NavigationScreenState();
+}
+
+class _NavigationScreenState extends State<NavigationScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> animation;
+
+  @override
+  void didUpdateWidget(NavigationScreen oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (oldWidget.iconData != widget.iconData) {
+      _startAnimation();
+    }
+  }
+
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+    );
+    animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+    _controller.forward();
+    super.initState();
+  }
+
+  _startAnimation() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000),
+    );
+    animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.easeIn,
+    );
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.brown,
+      child: ListView(
+        children: [
+          SizedBox(height: 64),
+          Center(
+            child: CircularRevealAnimation(
+              animation: animation,
+              centerOffset: Offset(80, 80),
+              maxRadius: MediaQuery.of(context).size.longestSide * 1.1,
+              child: Icon(
+                widget.iconData,
+                color: HexColor('#FFA400'),
+                size: 160,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HexColor extends Color {
+  HexColor(final String hexColor) : super(_getColorFromHex(hexColor));
+
+  static int _getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return int.parse(hexColor, radix: 16);
+  }
+}
